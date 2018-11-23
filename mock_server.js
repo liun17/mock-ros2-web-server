899d7af502b7827ffe8c5916ff2c911fa07a269d
you liang <tan_you_liang@hotmail.com>
2018-11-23 16:54:14 +0800

    change server.js

--- mock_server.js
+++ mock_server.js
@@ -34,6 +34,66 @@ msg1.requests = []
 
 
 
+app.use('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+app.set('port', process.env.PORT || 5003);
+
+
+app.use(express.json());
+
+
+app.use(express.static(__dirname + '/js'));
+
+
+app.use('/submit_task_msg', function (req, res) {
+	msg1.method = "createAdhocRequest";
+	msg1.payloads = ["ITEM" + Date.now().toString().substring(8, 15)];
+	msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+	all_messages = req.body;
+	for (var i = 0; i < all_messages.length; i++) {
+		console.log(all_messages[i].name + '  :  ' + all_messages[i].value);
+		if (all_messages[i].name == "pickup") {
+			//spec.fields.pickup=all_messages[i].value;
+			msg1.pickup = all_messages[i].value;
+			console.log("pickup" + msg1.pickup);
+		}
+		else if (all_messages[i].name == "delivery") {
+			msg1.delivery = all_messages[i].value;
+			console.log("delivery" + msg1.delivery);
+		}
+		msg.data = all_messages[i].value;
+	}
+	publisher.publish(msg1);
+	res.json('Request Submitted, yayyyyyy!!!!!!!');
+});
+
+
+app.use('/submit_status_msg', function (req, res) {
+	msg1.method = "getUserReqStates";
+	msg1.requests = []
+	publisher.publish(msg1);
+	res.json('Status Request Submitted');
+	console.log("submit status msg being called!!")
+});
+
+
+
+app.use('/send/', function (req, res) {
+	console.log("submit /send/ status msg being called!!")
+});
+
+
+
+app.listen(app.get('port'),function(){
+	console.log("Node Server running at port "+app.get('port'));
+});
+
+
+
+
 
 // RCL Nodejs Handler
 rclnodejs.init().then(() => {
@@ -61,54 +121,6 @@ rclnodejs.init().then(() => {
 	console.log("RCL Nodejs Init Successfully!!")
 	
 	rclnodejs.spin(node);
-	app.use('/', function (req, res) {
-		res.sendFile(__dirname + '/index.html');
-	});
-	
-	
-	app.set('port', process.env.PORT || 5003);
-	
-	
-	app.use(express.json());
-	
-	
-	app.use(express.static(__dirname + '/js'));
-	
-	
-	app.use('/submit_task_msg', function (req, res) {
-		msg1.method = "createAdhocRequest";
-		msg1.payloads = ["ITEM" + Date.now().toString().substring(8, 15)];
-		msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-		all_messages = req.body;
-		for (var i = 0; i < all_messages.length; i++) {
-			console.log(all_messages[i].name + '  :  ' + all_messages[i].value);
-			if (all_messages[i].name == "pickup") {
-				//spec.fields.pickup=all_messages[i].value;
-				msg1.pickup = all_messages[i].value;
-				console.log("pickup" + msg1.pickup);
-			}
-			else if (all_messages[i].name == "delivery") {
-				msg1.delivery = all_messages[i].value;
-				console.log("delivery" + msg1.delivery);
-			}
-			msg.data = all_messages[i].value;
-		}
-		publisher.publish(msg1);
-		res.json('Request Submitted, yayyyyyy!!!!!!!');
-	});
-	
-	
-	app.use('/submit_status_msg', function (req, res) {
-		msg1.method = "getUserReqStates";
-		msg1.requests = []
-		publisher.publish(msg1);
-		res.json('Status Request Submitted');
-		console.log("submit status msg being called!!")
-	});
 
-	app.listen(app.get('port'),function(){
-		console.log("Node Server running at port "+app.get('port'));
-	});
 
-	
 });
