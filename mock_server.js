a039077a2be457ce1cb01fe97b8b44c4777cd4f0
you liang <tan_you_liang@hotmail.com>
2018-11-23 17:09:42 +0800

    change server.js

--- mock_server.js
+++ mock_server.js
@@ -11,26 +11,26 @@
 var express = require('express');
 var app = express();
 
-const rclnodejs = require(__dirname + '/../rclnodejs/index.js');
-let String = rclnodejs.require('std_msgs').msg.String;
-let SestoApiInfo = rclnodejs.require('sesto_api_msgs').msg.SestoApiInfo;
-let ServerResponse = rclnodejs.require('sesto_api_msgs').msg.ServerResponse;
-let patientDevice_msg = rclnodejs.require('patient_device').msg.ServerResponse;
+// const rclnodejs = require(__dirname + '/../rclnodejs/index.js');
+// let String = rclnodejs.require('std_msgs').msg.String;
+// let SestoApiInfo = rclnodejs.require('sesto_api_msgs').msg.SestoApiInfo;
+// let ServerResponse = rclnodejs.require('sesto_api_msgs').msg.ServerResponse;
+// let patientDevice_msg = rclnodejs.require('patient_device').msg.ServerResponse;
 
-//	var date = new Date();
-let msg = new String();
-let msg1 = new SestoApiInfo();
-msg1.method = "createAdhocRequest";
-msg1.user_id = 1234;
-msg1.pickup = "";
-msg1.delivery = "";
-msg1.payloads = ["ITEM" + new Date().toISOString()];
-msg1.type = 0;
-msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-msg1.delivery_end = 30;
-msg1.agv_id = 0;
-msg1.req_id = 0;
-msg1.requests = []
+// //	var date = new Date();
+// let msg = new String();
+// let msg1 = new SestoApiInfo();
+// msg1.method = "createAdhocRequest";
+// msg1.user_id = 1234;
+// msg1.pickup = "";
+// msg1.delivery = "";
+// msg1.payloads = ["ITEM" + new Date().toISOString()];
+// msg1.type = 0;
+// msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+// msg1.delivery_end = 30;
+// msg1.agv_id = 0;
+// msg1.req_id = 0;
+// msg1.requests = []
 
 
 
@@ -39,46 +39,42 @@ app.use('/', function (req, res) {
 });
 
 
-app.set('port', process.env.PORT || 5003);
-
-
 app.use(express.json());
 
 
 app.use(express.static(__dirname + '/js'));
 
 
-app.use('/submit_task_msg', function (req, res) {
-	msg1.method = "createAdhocRequest";
-	msg1.payloads = ["ITEM" + Date.now().toString().substring(8, 15)];
-	msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-	all_messages = req.body;
-	for (var i = 0; i < all_messages.length; i++) {
-		console.log(all_messages[i].name + '  :  ' + all_messages[i].value);
-		if (all_messages[i].name == "pickup") {
-			//spec.fields.pickup=all_messages[i].value;
-			msg1.pickup = all_messages[i].value;
-			console.log("pickup" + msg1.pickup);
-		}
-		else if (all_messages[i].name == "delivery") {
-			msg1.delivery = all_messages[i].value;
-			console.log("delivery" + msg1.delivery);
-		}
-		msg.data = all_messages[i].value;
-	}
-	publisher.publish(msg1);
-	res.json('Request Submitted, yayyyyyy!!!!!!!');
-});
+// app.use('/submit_task_msg', function (req, res) {
+// 	msg1.method = "createAdhocRequest";
+// 	msg1.payloads = ["ITEM" + Date.now().toString().substring(8, 15)];
+// 	msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+// 	all_messages = req.body;
+// 	for (var i = 0; i < all_messages.length; i++) {
+// 		console.log(all_messages[i].name + '  :  ' + all_messages[i].value);
+// 		if (all_messages[i].name == "pickup") {
+// 			//spec.fields.pickup=all_messages[i].value;
+// 			msg1.pickup = all_messages[i].value;
+// 			console.log("pickup" + msg1.pickup);
+// 		}
+// 		else if (all_messages[i].name == "delivery") {
+// 			msg1.delivery = all_messages[i].value;
+// 			console.log("delivery" + msg1.delivery);
+// 		}
+// 		msg.data = all_messages[i].value;
+// 	}
+// 	publisher.publish(msg1);
+// 	res.json('Request Submitted, yayyyyyy!!!!!!!');
+// });
 
 
-app.use('/submit_status_msg', function (req, res) {
-	msg1.method = "getUserReqStates";
-	msg1.requests = []
-	publisher.publish(msg1);
-	res.json('Status Request Submitted');
-	console.log("submit status msg being called!!")
-});
-
+// app.use('/submit_status_msg', function (req, res) {
+// 	msg1.method = "getUserReqStates";
+// 	msg1.requests = []
+// 	publisher.publish(msg1);
+// 	res.json('Status Request Submitted');
+// 	console.log("submit status msg being called!!")
+// });
 
 
 app.get('/send/', function (req, res) {
@@ -86,9 +82,8 @@ app.get('/send/', function (req, res) {
 });
 
 
-
-app.listen(app.get('port'),function(){
-	console.log("Node Server running at port "+app.get('port'));
+app.listen( 5003 ,function(){
+	console.log("Node Server running at port "+ 5003);
 });
 
 
