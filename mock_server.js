3b6df5f50b0079fd1df252edeccdfc396a475731
you liang <tan_you_liang@hotmail.com>
2018-11-24 20:10:40 +0800

    trying to make it work

--- mock_server.js
+++ mock_server.js
@@ -78,72 +78,72 @@ rclnodejs.init().then(() => {
 	
 	rclnodejs.spin(node);
 	
-});
-
-
-app.use('/home', function (req, res) {
-	console.log("load main page");
-	res.sendFile(__dirname + '/index.html');
-});
-
-
-app.use(express.json());
-
-
-app.use(express.static(__dirname + '/js'));
-
-
-app.use('/submit_task_msg', function (req, res) {
-	msg1.method = "createAdhocRequest";
-	msg1.payloads = ["ITEM" + Date.now().toString().substring(8, 15)];
-	msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-	all_messages = req.body;
-	for (var i = 0; i < all_messages.length; i++) {
-		console.log(all_messages[i].name + '  :  ' + all_messages[i].value);
-		if (all_messages[i].name == "pickup") {
-			//spec.fields.pickup=all_messages[i].value;
-			msg1.pickup = all_messages[i].value;
-			console.log("pickup" + msg1.pickup);
+	
+	app.use('/home', function (req, res) {
+		console.log("load main page");
+		res.sendFile(__dirname + '/index.html');
+	});
+	
+	
+	app.use(express.json());
+	
+	
+	app.use(express.static(__dirname + '/js'));
+	
+	
+	app.use('/submit_task_msg', function (req, res) {
+		msg1.method = "createAdhocRequest";
+		msg1.payloads = ["ITEM" + Date.now().toString().substring(8, 15)];
+		msg1.start_time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+		all_messages = req.body;
+		for (var i = 0; i < all_messages.length; i++) {
+			console.log(all_messages[i].name + '  :  ' + all_messages[i].value);
+			if (all_messages[i].name == "pickup") {
+				//spec.fields.pickup=all_messages[i].value;
+				msg1.pickup = all_messages[i].value;
+				console.log("pickup" + msg1.pickup);
+			}
+			else if (all_messages[i].name == "delivery") {
+				msg1.delivery = all_messages[i].value;
+				console.log("delivery" + msg1.delivery);
+			}
+			msg.data = all_messages[i].value;
 		}
-		else if (all_messages[i].name == "delivery") {
-			msg1.delivery = all_messages[i].value;
-			console.log("delivery" + msg1.delivery);
-		}
-		msg.data = all_messages[i].value;
-	}
-	publisher.publish(msg1);
-	res.json('Request Submitted, yayyyyyy!!!!!!!');
-});
-
-
-app.use('/submit_status_msg', function (req, res) {
-	msg1.method = "getUserReqStates";
-	msg1.requests = []
-	publisher.publish(msg1);
-	res.json('Status Request Submitted');
-	console.log("submit status msg being called!!")
-});
-
-
-app.get('/send', function (req, res) {
-	console.log("submit /send/ status msg being called!!")
-	setInterval(function(){ 
+		publisher.publish(msg1);
+		res.json('Request Submitted, yayyyyyy!!!!!!!');
+	});
+	
+	
+	app.use('/submit_status_msg', function (req, res) {
+		msg1.method = "getUserReqStates";
+		msg1.requests = []
+		publisher.publish(msg1);
+		res.json('Status Request Submitted');
+		console.log("submit status msg being called!!")
+	});
+	
+	
+	app.get('/send', function (req, res) {
+		console.log("submit /send/ status msg being called!!")
+		setInterval(function(){ 
+			ack_msg.status = req.params.status
+			ack_msg.deviceid = "AAAA"
+			patientDevice_pub.publish(ack_msg);
+			console.log("pub to /call_acknowledgment"); 
+		}, 3000);
+		
+	});
+	
+	app.get('/ack/:status', function(req, res) {
+		// res.send(req.params.status);
 		ack_msg.status = req.params.status
 		ack_msg.deviceid = "AAAA"
 		patientDevice_pub.publish(ack_msg);
-		console.log("pub to /call_acknowledgment"); 
-	}, 3000);
+		console.log("!!!!!!Reply status to topic as ", req.params.status)
+	});
 	
-});
-
-app.get('/ack/:status', function(req, res) {
-	// res.send(req.params.status);
-	ack_msg.status = req.params.status
-	ack_msg.deviceid = "AAAA"
-	patientDevice_pub.publish(ack_msg);
-	console.log("!!!!!!Reply status to topic as ", req.params.status)
-});
-
-app.listen( 5003 ,function(){
-	console.log("Node Server running at port "+ 5003);
+	app.listen( 5003 ,function(){
+		console.log("Node Server running at port "+ 5003);
+	});
+	
 });
